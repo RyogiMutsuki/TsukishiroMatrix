bb2dc17687d46a5abdb50a7821858b9a0f71c739
TianyiMatrix <tianyi@matrix.app>
2026-07-30 23:27:23 +0800

    easter egg 1: idle status bar cycling 好饿好饿 messages

--- Tsukishiro.MatrixApp/ViewModels/MainWindowViewModel.cs
+++ Tsukishiro.MatrixApp/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,51 @@
-﻿namespace Tsukishiro.MatrixApp.ViewModels;
+﻿using System.Timers;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Tsukishiro.MatrixApp.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    [ObservableProperty]
+    private string _statusMessage = "Disconnected";
+
+    [ObservableProperty]
+    private string _titleText = "Tsukishiro MatrixApp";
+
+    private readonly string[] _idleMessages =
+    [
+        "好饿好饿好饿我真的好饿",
+        "吃呀吃呀吃呀吃",
+        "锦依卫时刻守护",
+        "66CCFF",
+        "华风夏韵，洛水天依",
+        "Connected"
+    ];
+
+    private int _idleIndex;
+    private readonly Timer _idleTimer;
+    private bool _isIdle;
+
+    public MainWindowViewModel()
+    {
+        _idleTimer = new Timer(8000);
+        _idleTimer.Elapsed += (_, _) => CycleIdleMessage();
+        _idleTimer.AutoReset = true;
+        _idleTimer.Start();
+    }
+
+    private void CycleIdleMessage()
+    {
+        _isIdle = true;
+        _idleIndex = (_idleIndex + 1) % _idleMessages.Length;
+        StatusMessage = _idleMessages[_idleIndex];
+    }
+
+    partial void OnStatusMessageChanged(string value)
+    {
+        if (value == "Connected")
+        {
+            _isIdle = false;
+        }
+    }
 }
